e678d126576ba1b50bc9924f53fc056235037637
Yiheng Xiong <11613021@mail.sustc.edu.cn>
2024-01-07 13:46:17 +0100

    update markor

--- properties/markor/markor_new.py
+++ properties/markor/markor_new.py
@@ -320,31 +320,31 @@ class Test(AndroidCheck):
     #     assert "#" not in self.device(className="android.webkit.WebView").child(className="android.view.View").info["contentDescription"], "2 markdown format failed"
 
     # bug #1020
-    @precondition(
-        lambda self: self.device(resourceId="net.gsantner.markor:id/new_file_dialog__name").exists() 
-        )
-    @rule()
-    def file_type_should_be_the_same(self):
-        file_type = self.device(resourceId="net.gsantner.markor:id/new_file_dialog__type").child(className="android.widget.TextView").get_text()
-        print("file_type: " + file_type)
-        file_name_suffix = self.device(resourceId="net.gsantner.markor:id/new_file_dialog__ext").get_text()
-        print("file_name_suffix: " + file_name_suffix)
-        if file_type == "Markdown":
-            assert file_name_suffix == ".md"
-        elif file_type == "Plain Text":
-            assert file_name_suffix == ".txt"
-        elif file_type == "todo.txt":
-            assert file_name_suffix == ".todo.txt"
-        elif file_type == "AsciiDoc":
-            assert file_name_suffix == ".adoc"
-        elif file_type == "CSV":
-            assert file_name_suffix == ".csv"
-        elif file_type == "OrgMode":
-            assert file_name_suffix == ".org"
-        elif file_type == "Wikitext":
-            assert file_name_suffix == ".txt"
-        else:
-            assert file_name_suffix == ".md"
+    # @precondition(
+    #     lambda self: self.device(resourceId="net.gsantner.markor:id/new_file_dialog__name").exists() 
+    #     )
+    # @rule()
+    # def file_type_should_be_the_same(self):
+    #     file_type = self.device(resourceId="net.gsantner.markor:id/new_file_dialog__type").child(className="android.widget.TextView").get_text()
+    #     print("file_type: " + file_type)
+    #     file_name_suffix = self.device(resourceId="net.gsantner.markor:id/new_file_dialog__ext").get_text()
+    #     print("file_name_suffix: " + file_name_suffix)
+    #     if file_type == "Markdown":
+    #         assert file_name_suffix == ".md"
+    #     elif file_type == "Plain Text":
+    #         assert file_name_suffix == ".txt"
+    #     elif file_type == "todo.txt":
+    #         assert file_name_suffix == ".todo.txt"
+    #     elif file_type == "AsciiDoc":
+    #         assert file_name_suffix == ".adoc"
+    #     elif file_type == "CSV":
+    #         assert file_name_suffix == ".csv"
+    #     elif file_type == "OrgMode":
+    #         assert file_name_suffix == ".org"
+    #     elif file_type == "Wikitext":
+    #         assert file_name_suffix == ".txt"
+    #     else:
+    #         assert file_name_suffix == ".md"
 
     #bug 994
     @precondition(
