c1f89b910eeebf3098906ffbe8de563d1142eb90
Yiheng Xiong <11613021@mail.sustc.edu.cn>
2024-01-23 16:15:41 +0100

    omninotes 237

---
+++ properties/omninotes/omninotes_237.py
@@ -0,0 +1,144 @@
+import string
+import sys
+import time
+sys.path.append("..")
+from main import *
+
+class Test(AndroidCheck):
+    def __init__(
+        self,
+        apk_path,
+        device_serial="emulator-5554",
+        output_dir="output",
+        explore_event_count=5000,
+        diverse_event_count=5000,
+        main_path_path=None,
+        xml_path="None",
+        source_activity=None,
+        target_activity=None,
+        policy_name="pbt",
+        timeout=-1,
+        build_model_timeout=-1
+    ):
+        super().__init__(
+            apk_path,
+            device_serial=device_serial,
+            output_dir=output_dir,
+            explore_event_count=explore_event_count,
+            diverse_event_count=diverse_event_count,
+            xml_path=xml_path,
+            main_path_path=main_path_path,
+            source_activity=source_activity,
+            target_activity=target_activity,
+            policy_name=policy_name,
+            timeout=timeout,
+            build_model_timeout=build_model_timeout
+        )
+
+    @initialize()
+    def set_up(self):
+        self.device(resourceId="it.feio.android.omninotes:id/next").click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/next").click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/next").click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/next").click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/next").click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/done").click()
+        time.sleep(1)
+        if self.device(text="OK").exists():
+            self.device(text="OK").click()
+            time.sleep(1)
+        # 打开设置-在navigation 中显示没有被分类的Notes
+        # self.device(description="drawer open").click()
+        # time.sleep(1)
+        # self.device(text="SETTINGS").click()
+        # time.sleep(1)
+        # self.device(text="Navigation").click()
+        # time.sleep(1)
+        # self.device(text="Group not categorized").click()
+        # time.sleep(1)
+        # self.device(description="Navigate up").click()
+        # time.sleep(1)
+        # self.device(description="Navigate up").click()
+        # time.sleep(1)
+        # self.device.press("back")
+        # time.sleep(1)
+        # # 创建一个新的Note
+        # self.device(resourceId="it.feio.android.omninotes:id/fab_expand_menu_button").click()
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/fab_note").click()
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/detail_title").set_text("test")
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/detail_content").set_text("#bb")
+        # time.sleep(1)
+        # # 添加新的category
+        # self.device(resourceId="it.feio.android.omninotes:id/menu_category").click()
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/buttonDefaultPositive").click()
+        # time.sleep(1)
+        # category_name = st.text(alphabet=string.printable,min_size=1, max_size=10).example()
+        # self.device(resourceId="it.feio.android.omninotes:id/category_title").set_text(category_name)
+        # time.sleep(1)
+        # self.device(text="OK").click()
+        # self.device(description="drawer closed").click()
+    
+    @precondition(lambda self: self.device(resourceId="it.feio.android.omninotes:id/menu_attachment").exists() and self.device(resourceId="it.feio.android.omninotes:id/menu_share").exists() and self.device(resourceId="it.feio.android.omninotes:id/menu_tag").exists() )
+    @rule()
+    def hash_tag_with_number_start_shouldbe_recognized(self):
+        print("time: " + str(time.time() - start_time))
+        text = st.text(alphabet=string.digits+string.ascii_letters,min_size=1, max_size=5).example()
+        tag = "#"+ text
+        print("tag: " + tag)
+        self.device(resourceId="it.feio.android.omninotes:id/detail_content").set_text(tag)
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/toolbar").child(className="android.widget.ImageButton").click()
+        time.sleep(1)
+
+        note_count = int(self.device(resourceId="it.feio.android.omninotes:id/list").child(resourceId="it.feio.android.omninotes:id/root").count)
+        selected_note = random.randint(0, note_count - 1)
+        selected_note_name = self.device(resourceId="it.feio.android.omninotes:id/note_title")[selected_note].info['text']
+        print("selected_note: " + str(selected_note_name))
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/list").child(resourceId="it.feio.android.omninotes:id/root")[selected_note].click()
+        time.sleep(1)
+        self.device(resourceId="it.feio.android.omninotes:id/menu_tag").click()
+
+        assert self.device(resourceId="it.feio.android.omninotes:id/title",textContains=text).exists()
+
+start_time = time.time()
+
+# args = sys.argv[1:]
+# apk_path = args[0]
+# device_serial = args[1]
+# output_dir = args[2]
+# xml_path = args[3]
+# main_path_path = args[4]
+# source_activity = args[5]
+# target_activity = args[6]
+# policy_name = args[7]
+# t = Test(
+#     apk_path="./apk/AnkiDroid-2.15.2.apk",
+#     device_serial="emulator-5554",
+#     output_dir="output/anki/random2",
+#     event_count=1000,
+#     xml_path="./xml_graph/Anki_CTG.xml",
+#     source_activity="DeckPicker",
+#     target_activity="Preferences",
+#     policy_name="random", dfs_greedy
+# )
+t = Test(
+    apk_path="./apk/omninotes/OmniNotes-5.1.0.apk",
+    device_serial="emulator-5554",
+    output_dir="output/omninotes/237/1",
+    policy_name="random",
+    timeout=7200,
+    build_model_timeout=3600
+)
+t.start()
+execution_time = time.time() - start_time
+print("execution time: " + str(execution_time))
