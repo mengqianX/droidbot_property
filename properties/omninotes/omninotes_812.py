5d1eed14519b8e685c71d0f314d23d357bda8eb5
Yiheng Xiong <11613021@mail.sustc.edu.cn>
2024-01-24 15:40:07 +0100

    812

--- properties/omninotes/omninotes_812.py
+++ properties/omninotes/omninotes_812.py
@@ -10,13 +10,15 @@ class Test(AndroidCheck):
         apk_path,
         device_serial="emulator-5554",
         output_dir="output",
-        explore_event_count=0,
-        diverse_event_count=100,
+        explore_event_count=5000,
+        diverse_event_count=5000,
         main_path_path=None,
         xml_path="None",
         source_activity=None,
         target_activity=None,
         policy_name="pbt",
+        timeout=-1,
+        build_model_timeout=-1
     ):
         super().__init__(
             apk_path,
@@ -29,6 +31,8 @@ class Test(AndroidCheck):
             source_activity=source_activity,
             target_activity=target_activity,
             policy_name=policy_name,
+            timeout=timeout,
+            build_model_timeout=build_model_timeout
         )
 
     @initialize()
@@ -46,39 +50,39 @@ class Test(AndroidCheck):
         self.device(resourceId="it.feio.android.omninotes:id/done").click()
         time.sleep(1)
         # 打开设置-在navigation 中显示没有被分类的Notes
-        self.device(description="drawer open").click()
-        time.sleep(1)
-        self.device(text="SETTINGS").click()
-        time.sleep(1)
-        self.device(text="Navigation").click()
-        time.sleep(1)
-        self.device(text="Group not categorized").click()
-        time.sleep(1)
-        self.device(description="Navigate up").click()
-        time.sleep(1)
-        self.device(description="Navigate up").click()
-        time.sleep(1)
-        self.device.press("back")
-        time.sleep(1)
-        # 创建一个新的Note
-        self.device(resourceId="it.feio.android.omninotes:id/fab_expand_menu_button").click()
-        time.sleep(1)
-        self.device(text="Text note").click()
-        time.sleep(1)
-        self.device(resourceId="it.feio.android.omninotes:id/detail_title").set_text("test")
-        time.sleep(1)
-        self.device(resourceId="it.feio.android.omninotes:id/detail_content").set_text("#bb")
-        time.sleep(1)
-        # 添加新的category
-        self.device(resourceId="it.feio.android.omninotes:id/menu_category").click()
-        time.sleep(1)
-        self.device(resourceId="it.feio.android.omninotes:id/buttonDefaultPositive").click()
-        time.sleep(1)
-        category_name = st.text(alphabet=string.printable,min_size=1, max_size=10).example()
-        self.device(resourceId="it.feio.android.omninotes:id/category_title").set_text(category_name)
-        time.sleep(1)
-        self.device(text="OK").click()
-        self.device(description="drawer closed").click()
+        # self.device(description="drawer open").click()
+        # time.sleep(1)
+        # self.device(text="SETTINGS").click()
+        # time.sleep(1)
+        # self.device(text="Navigation").click()
+        # time.sleep(1)
+        # self.device(text="Group not categorized").click()
+        # time.sleep(1)
+        # self.device(description="Navigate up").click()
+        # time.sleep(1)
+        # self.device(description="Navigate up").click()
+        # time.sleep(1)
+        # self.device.press("back")
+        # time.sleep(1)
+        # # 创建一个新的Note
+        # self.device(resourceId="it.feio.android.omninotes:id/fab_expand_menu_button").click()
+        # time.sleep(1)
+        # self.device(text="Text note").click()
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/detail_title").set_text("test")
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/detail_content").set_text("#bb")
+        # time.sleep(1)
+        # # 添加新的category
+        # self.device(resourceId="it.feio.android.omninotes:id/menu_category").click()
+        # time.sleep(1)
+        # self.device(resourceId="it.feio.android.omninotes:id/buttonDefaultPositive").click()
+        # time.sleep(1)
+        # category_name = st.text(alphabet=string.printable,min_size=1, max_size=10).example()
+        # self.device(resourceId="it.feio.android.omninotes:id/category_title").set_text(category_name)
+        # time.sleep(1)
+        # self.device(text="OK").click()
+        # self.device(description="drawer closed").click()
     
     @precondition(lambda self: self.device(resourceId="it.feio.android.omninotes:id/menu_search").exists() and self.device(resourceId="it.feio.android.omninotes:id/note_title").exists() and self.device(text="Notes").exists() and not self.device(text="SETTINGS").exists())
     @rule()
@@ -156,12 +160,12 @@ start_time = time.time()
 #     policy_name="random", dfs_greedy
 # )
 t = Test(
-    apk_path="./apk/OmniNotes-6.0.5.apk",
+    apk_path="./apk/omninotes/OmniNotes-6.0.5.apk",
     device_serial="emulator-5554",
     output_dir="output/omninotes/812/1",
-    explore_event_count=500,
-    diverse_event_count=500,
     policy_name="random",
+    timeout=7200,
+    build_model_timeout=3600
 )
 t.start()
 execution_time = time.time() - start_time
